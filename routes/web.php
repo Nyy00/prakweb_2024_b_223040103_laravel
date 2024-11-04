6ed7caf7f674dda0db09a1c98592134b33f62d08
Nyy00 <donylks111@gmail.com>
2024-11-04 20:36:39 +0700

    vid 5

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/home', function () {
+    return view('home',['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Dony Laksmana']);
+    return view('about', ['nama' => 'Dony Laksmana'], ['title' => 'Contact']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog',['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact',['title' => 'Contact']);
 });
